a2d41870c627eb64511e429681f7d350afaa76a0
Marco Antonio Rosa da Silva <marco.silva@unochapeco.edu.br>
2023-05-05 20:22:38 -0300

    added login route

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import home from './components/home.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <home />
 
       <nav>
+        <p>This is a Nav</p>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
---
+++ front/src/views/home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Home from '../components/Home.vue'
+</script>
+
+<template>
+  <main>
+    <Home msg="Home sweet home."/>
+  </main>
+</template>
